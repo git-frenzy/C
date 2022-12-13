affd77d6928e160f51ce2ea5678c44e9b6b73fc3
Kanishk Suri <kanishksuri29@gmail.com>
2022-12-14 03:00:34 +0530

    update

--- untitled folder/main.c
+++ untitled folder/main.c
@@ -1,9 +1,22 @@
-#include <stdio.h>
+#include <stdio.
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
-
+    int x, y, prime;
+    for(x=2; x<50; x++){
+        if(x==2){
+            prime = x;
+            printf("%d", prime);
+        } else{
+            for (y = 2; y < x; y++) {
+                if(y%x == 0){
+                    break;
+                } else{
+                    prime =x;
+                    printf("%d", prime);
+                }
+            }
+        }
+    }
 
 
 }
